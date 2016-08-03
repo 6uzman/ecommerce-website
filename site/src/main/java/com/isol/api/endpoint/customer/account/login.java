ca33c2761922d434c0a4c5aa8819bf6e064cd371
Miguel Guzman <jose.guzman@isol.mx>
2016-08-03 18:41:28 -0500

    Initial commit

---
+++ site/src/main/java/com/isol/api/endpoint/customer/account/login.java
@@ -0,0 +1,8 @@
+package com.mycompany.api.endpoint.customer.account;
+
+/**
+ * Created by Gerardo_isol on 19/07/2016.
+ */
+public class login extends org.broadleafcommerce.core.web.api.endpoint.customer.CustomerEndpoint {
+
+}
